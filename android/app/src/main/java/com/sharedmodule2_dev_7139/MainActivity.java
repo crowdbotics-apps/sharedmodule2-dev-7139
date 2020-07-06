511da2f760a9be1f6c7fa1627fa624d4ace31362
Crowdbotics <team@crowdbotics.com>
2020-07-06 19:13:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sharedmodule2_dev_7139/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sharedmodule2_dev_7139;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "sharedmodule2_dev_7139";
+    }
+}
